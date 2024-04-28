185ce235ae38137641f45f78d59ae87531b1884f
Heber Uchytil <heberespinoza2200@gmail.com>
2024-04-27 19:14:49 -0600

    finished my first assignment and got the hand of it

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name?");
+        string fname = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
