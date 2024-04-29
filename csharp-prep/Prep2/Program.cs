e807057af5ba5d0c0fbd89c52cd583a0b8d8d3e9
Heber Uchytil <heberespinoza2200@gmail.com>
2024-04-29 07:36:58 -0600

    finished the prep 2 garde program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is the precentage of your grade?");
+        string user_answer = Console.ReadLine();
+        int percent = int.Parse(user_answer);
+        string Grade_letter = "";
+
+        if (percent >= 90)
+            {
+                Grade_letter = "A";
+            } 
+        else if(percent >= 80)
+            {
+                Grade_letter = "B";
+            }
+        else if (percent >= 70)
+            {
+                Grade_letter = "C";
+            }
+        else if (percent >= 60)
+            {
+                Grade_letter = "D";
+            }
+        else
+            {
+                Grade_letter = "F";
+            }
+        
+        Console.WriteLine($"Your grade is :{Grade_letter}");
+
+        if (percent >= 70)
+            {
+                Console.WriteLine("You passed the class!!");
+            }
+         else
+            {
+                Console.WriteLine("You have failed, better luck next time :)");
+            }
     }
 }
