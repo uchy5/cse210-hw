56b30551ce029ce4d7335a3e44d000568ad09d3e
Heber Uchytil <heberespinoza2200@gmail.com>
2024-05-01 09:54:39 -0600

    magic number game made and completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+    Random randomGenerator = new Random();
+    int MagicNumber = randomGenerator.Next(1, 101);
+
+    int UserGuess = -1;
+
+    while (UserGuess != MagicNumber)
+    {
+        Console.Write("what is your guess?");
+        UserGuess = int.Parse(Console.ReadLine());
+        if (MagicNumber > UserGuess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (MagicNumber < UserGuess)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else
+        {
+            Console.WriteLine("you guessed the magic number");
+        }
     }
+    
+
+    }
+
 }
